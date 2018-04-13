ba1bce40e27e272ca03479054f264a7531469815
Vijay Singh <vijay.singh@ecollabs.com>
2018-04-13 15:27:40 +0530

    Cleanup 3.

--- FileUpload/saveFiles.ashx.cs
+++ FileUpload/saveFiles.ashx.cs
@@ -13,46 +13,46 @@ using System.IO;
 
 namespace SyncfusionUploader1
 {
-    /// <summary>
-    /// Summary description for saveFiles
-    /// </summary>
-    public class saveFiles : IHttpHandler
-    {
+	/// <summary>
+	/// Summary description for saveFiles
+	/// </summary>
+	public class saveFiles : IHttpHandler
+	{
 
-        public void ProcessRequest(HttpContext context)
-        {
-            string targetFolder = HttpContext.Current.Server.MapPath("uploadfiles");
-            if (!Directory.Exists(targetFolder))
-            {
-                Directory.CreateDirectory(targetFolder);
-            }
-            HttpRequest request = context.Request;
-            HttpFileCollection uploadedFiles = context.Request.Files;
-            if (uploadedFiles != null && uploadedFiles.Count > 0)
-            {
-                for (int i = 0; i < uploadedFiles.Count; i++)
-                {
-                    string fileName = uploadedFiles[i].FileName;
-                    int indx = fileName.LastIndexOf("\\");
-                    if (indx > -1)
-                    {
-                        fileName = fileName.Substring(indx + 1);
-                    }
-                    //uploadedFiles[i].SaveAs(targetFolder + "\\" + fileName);
-                }
-            }
-            else
-            {
+		public void ProcessRequest(HttpContext context)
+		{
+			string targetFolder = HttpContext.Current.Server.MapPath("uploadfiles");
+			if (!Directory.Exists(targetFolder))
+			{
+				Directory.CreateDirectory(targetFolder);
+			}
+			HttpRequest request = context.Request;
+			HttpFileCollection uploadedFiles = context.Request.Files;
+			if (uploadedFiles != null && uploadedFiles.Count > 0)
+			{
+				for (int i = 0; i < uploadedFiles.Count; i++)
+				{
+					string fileName = uploadedFiles[i].FileName;
+					int indx = fileName.LastIndexOf("\\");
+					if (indx > -1)
+					{
+						fileName = fileName.Substring(indx + 1);
+					}
+					uploadedFiles[i].SaveAs(targetFolder + "\\" + fileName);
+				}
+			}
+			else
+			{
 
-            }
-        }
+			}
+		}
 
-        public bool IsReusable
-        {
-            get
-            {
-                return false;
-            }
-        }
-    }
+		public bool IsReusable
+		{
+			get
+			{
+				return false;
+			}
+		}
+	}
 }
